ac6c9a13860963e43af39082b71dbbf85298d08d
veliulugut <veliulugut1@gmail.com>
2024-01-31 12:08:05 +0300

    tables test

--- pkg/repository/entadp/tablest_test.go
+++ pkg/repository/entadp/tablest_test.go
@@ -67,3 +67,43 @@ func TestTable_DeleteTable(t *testing.T) {
 	}
 
 }
+
+func TestTable_UpdateTable(t *testing.T) {
+	opts := []enttest.Option{
+		enttest.WithOptions(ent.Log(t.Log)),
+	}
+	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&_fk=1", opts...)
+	defer client.Close()
+
+	tableRepo := NewTablesRepository(client)
+
+	cd := dto.Tables{
+		NumberOfGuests: 3,
+		TableNumber:    1,
+		Description:    "test",
+		TableType:      "vip",
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
+	}
+
+	err := tableRepo.CreateTable(context.Background(), &cd)
+	if err != nil {
+		t.Error(err)
+	}
+
+	updateTables := dto.Tables{
+		NumberOfGuests: 4,
+		TableNumber:    2,
+		Description:    "test",
+		TableType:      "default",
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
+	}
+
+	t.Run("UpdateTable", func(t *testing.T) {
+		err := tableRepo.UpdateTable(context.Background(), 1, &updateTables)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+}
