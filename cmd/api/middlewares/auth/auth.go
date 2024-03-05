49cefd3cdfcc74aa4c33ae34e4e03ae94eed374b
veliulugut <veliulugut1@gmail.com>
2024-03-05 23:43:20 +0300

    add auth middlewares

---
+++ cmd/api/middlewares/auth/auth.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"cafe-management/pkg/jwt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var _ Auth = (*Middleware)(nil)
+
+func New(j jwt.Interface) *Middleware {
+	return &Middleware{
+		jwt: j,
+	}
+}
+
+type Middleware struct {
+	jwt jwt.Interface
+}
+
+func (mw *Middleware) Check() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		a := c.Request.Header.Get("Authorization")
+		if a == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "authorization field not found"})
+			c.Abort()
+			return
+		}
+		token := strings.Replace(a, "Bearer ", "", 1)
+		_, err := mw.jwt.Parse(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+	}
+}
