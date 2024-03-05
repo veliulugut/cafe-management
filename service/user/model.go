eceb3497e1e1eb16a9dc1a487b21078f952ada64
veliulugut <veliulugut1@gmail.com>
2024-03-05 23:48:40 +0300

    remove unnecesarry struct fields

--- service/user/model.go
+++ service/user/model.go
@@ -18,14 +18,9 @@ type UpdateUserModel struct {
 }
 
 type UserModel struct {
-	UserID    int       `json:"user_id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Password  string    `json:"password"`
 	UserName  string    `json:"user_name"`
 	Avatar    string    `json:"avatar"`
 	Phone     string    `json:"phone"`
 	Email     string    `json:"email"`
-	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
 }
