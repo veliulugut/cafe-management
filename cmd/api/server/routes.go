44ac35db5a4d8144de43308c63c7c0875d9644df
veliulugut <veliulugut1@gmail.com>
2024-03-07 21:41:43 +0300

    add register api

--- cmd/api/server/routes.go
+++ cmd/api/server/routes.go
@@ -3,6 +3,7 @@ package server
 func (s *Server) linkRoutes() {
 	v1Grp := s.router.Group("/v1")
 	v1Grp.POST("/login", s.hnd.login.Login)
+	v1Grp.POST("/register", s.hnd.login.Register)
 	v1Grp.POST("/user", s.hnd.user.CreateUser)
 
 }
