9140484b37769a49f95fe6f3f9db10dc43c622a4
veliulugut <veliulugut1@gmail.com>
2024-03-07 21:40:45 +0300

    add register

--- service/login/interface.go
+++ service/login/interface.go
@@ -4,4 +4,5 @@ import "context"
 
 type Service interface {
 	Login(ctx context.Context, username, password string) (string, error)
+	Register(ctx context.Context, firstName, lastName, email, password, confirmPassword string) (string, error)
 }
--- service/login/login.go
+++ service/login/login.go
@@ -3,6 +3,7 @@ package login
 import (
 	"cafe-management/pkg/jwt"
 	"cafe-management/pkg/passwd"
+	"cafe-management/pkg/repository/dto"
 	"cafe-management/pkg/repository/entadp"
 	"context"
 	"fmt"
@@ -38,6 +39,44 @@ func (l *Login) Login(ctx context.Context, username, password string) (string, e
 	token, err := l.jwt.Generate(int(user.UserID))
 	if err != nil {
 		return "", fmt.Errorf("login :%w", err)
+	}
+
+	return token, nil
+}
+
+func (l *Login) Register(ctx context.Context, firstName, lastName, email, password, confirmPassword string) (string, error) {
+	user := &dto.User{
+		FirstName:       firstName,
+		LastName:        lastName,
+		Email:           email,
+		Password:        password,
+		ConfirmPassword: confirmPassword,
+	}
+
+	_, err := l.repo.User().GetByUserName(ctx, user.UserName)
+	if err == nil {
+		return "", fmt.Errorf("username %s is already taken", user.UserName)
+	}
+
+	if password != confirmPassword {
+		return "", fmt.Errorf("passwords don't match")
+	}
+
+	hashedPassword, err := l.bc.Generate(password)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	user.Password = hashedPassword
+
+	err = l.repo.User().CreateUser(ctx, user)
+	if err != nil {
+		return "", fmt.Errorf("failed to create user: %w", err)
+	}
+
+	token, err := l.jwt.Generate(int(user.UserID))
+	if err != nil {
+		return "", fmt.Errorf("failed to generate token: %w", err)
 	}
 
 	return token, nil
